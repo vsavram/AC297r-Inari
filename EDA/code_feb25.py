1087ed070523d82dbdfc78b1ecc76a54ee63386a
winterbreeze23 <37491296+winterbreeze23@users.noreply.github.com>
2021-02-25 21:24:34 -0500

    Update code_feb25.py

--- EDA/code_feb25.py
+++ EDA/code_feb25.py
@@ -28,4 +28,14 @@ desc = data.describe(percentiles = perc, include = include)
 # display 
 desc 
 
-df_genes = data.drop(columns=['Run', 'growth_condition', 'Cultivar', 'Developmental_stage', 'organism_part', 'Age'])
+data = data.set_index('Run')
+data = data.drop(columns=['growth_condition', 'Cultivar', 'Developmental_stage', 'organism_part', 'Age'])
+
+max_gene_level = data.max(axis=0)
+print('Max gene expression level of all genes:', max_gene_level.max())
+print('Max gene expression levels by quantile:', max_gene_level.quantile([.1, .15, .2, .25, .3, .5, .75, .8, .9, 1]))
+
+print('Number of genes whose max expression level is 0:', max_gene_level.isin([0]).sum())
+print('Number of genes whose max expression level is less than 1 TPM:', max_gene_level[max_gene_level < 1.].count())
+print('Number of genes whose max expression level is less than 5 TPM:', max_gene_level[max_gene_level < 5.].count())
+print('Number of genes whose max expression level is less than 10 TPM:', max_gene_level[max_gene_level < 10.].count())
